3b0b3a929963b91eded10f908a9d5469c4fe597a
louisegarros <103496057+louisegarros@users.noreply.github.com>
2022-04-19 11:27:05 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,47 @@
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . # # # .
+    # . # . #
+    . . # . .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    # . # . #
+    . . # . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . # . .
+    . . # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(200)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
